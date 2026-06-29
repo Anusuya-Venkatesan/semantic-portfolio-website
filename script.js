3f31e7b2a6046035647e99c38493f65cacf98b7c
ANUSUYA <anusuya62566@gmail.com>
2026-06-29 12:16:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,24 @@
-const apiKey = "fb3870359d0986caac08c0fd31949e60";
+const apiKey = "YOUR_API_KEY";
 
-const cityInput =
-document.getElementById("cityInput");
+async function getWeather() {
 
-const searchBtn =
-document.getElementById("searchBtn");
+    const city = document.getElementById("city").value;
 
-const weatherResult =
-document.getElementById("weatherResult");
+    const weatherDiv = document.getElementById("weather");
 
-async function getWeather(city){
+    weatherDiv.innerHTML = "Loading...";
 
-    try{
-
-        weatherResult.innerHTML =
-        "<p>Loading...</p>";
+    try {
 
         const response = await fetch(
 `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
         );
 
         if (!response.ok) {
-    const errorData = await response.json();
-    throw new Error(errorData.message);
-}
-        const data =
-        await response.json();
+            throw new Error("City not found");
+        }
+
+        const data = await response.json();
 
         displayWeather(data);
 
@@ -33,57 +26,35 @@ async function getWeather(city){
 
     catch(error){
 
-        weatherResult.innerHTML =
+        weatherDiv.innerHTML =
         `<p>${error.message}</p>`;
+
     }
+
 }
 
 function displayWeather(data){
 
-    weatherResult.innerHTML = `
-        <h2>${data.name}</h2>
+const weatherDiv =
+document.getElementById("weather");
 
-        <p>
-            Temperature:
-            ${data.main.temp} °C
-        </p>
+weatherDiv.innerHTML=`
 
-        <p>
-            Humidity:
-            ${data.main.humidity}%
-        </p>
+<h2>${data.name},
+${data.sys.country}</h2>
 
-        <p>
-            Wind Speed:
-            ${data.wind.speed} m/s
-        </p>
+<p><b>Temperature:</b>
+${data.main.temp} °C</p>
 
-        <p>
-            Weather:
-            ${data.weather[0].description}
-        </p>
-    `;
+<p><b>Humidity:</b>
+${data.main.humidity}%</p>
+
+<p><b>Wind Speed:</b>
+${data.wind.speed} m/s</p>
+
+<p><b>Condition:</b>
+${data.weather[0].description}</p>
+
+`;
+
 }
-
-searchBtn.addEventListener(
-    "click",
-    () => {
-
-        const city =
-        cityInput.value.trim();
-
-        if(city !== ""){
-            getWeather(city);
-        }
-    }
-);
-
-cityInput.addEventListener(
-    "keypress",
-    (e) => {
-
-        if(e.key === "Enter"){
-            searchBtn.click();
-        }
-    }
-);
